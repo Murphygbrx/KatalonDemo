d19c212872d3420083636c79c9a29fd8e7b77b4c
patrick.murphy <patrick.murphy@BOU-CH78ST2.corp.gbrx.com>
2019-03-11 14:37:57 -0600

    Testing that things work

--- Test Suites/Suite Alpha/Smith Login.ts
+++ Test Suites/Suite Alpha/Smith Login.ts
@@ -11,15 +11,15 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>019840e8-dd74-4508-92ac-e2a72d0d1870</testSuiteGuid>
    <testCaseLink>
-      <guid>e53f9b3b-99e9-420e-8d13-691bfda8ead7</guid>
+      <guid>85fdcd1a-0d55-4ce3-a60e-da7c8e181f11</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Smith Failed Login</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>85fdcd1a-0d55-4ce3-a60e-da7c8e181f11</guid>
+      <guid>d0a6ac2a-66fb-491d-971d-9af5b90a16af</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Smith Failed Login</testCaseId>
+      <testCaseId>Test Cases/Glen01Login</testCaseId>
    </testCaseLink>
 </TestSuiteEntity>

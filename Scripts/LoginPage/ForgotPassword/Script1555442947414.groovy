6c687c50ccf3890b5095cfc04204268af507e86a
patrick.murphy <patrick.murphy@BOU-CH78ST2.corp.gbrx.com>
2019-04-22 11:17:09 -0600

    Fixed and completed Forgot Password Test. 2855

--- Scripts/LoginPage/ForgotPassword/Script1555442947414.groovy
+++ Scripts/LoginPage/ForgotPassword/Script1555442947414.groovy
@@ -1,46 +1,40 @@
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-
-import java.sql.ResultSet
-
+import java.sql.ResultSet as ResultSet
 import com.kms.katalon.core.model.FailureHandling as FailureHandling
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import internal.GlobalVariable as GlobalVariable
 
-try {
-/*	CustomKeywords.'com.database.DemoMySql.connectDB'('tst-ensp-sql06\\gbx17', 'sa', 'boulder.1', 'Enspire')
-	ResultSet rowsa = CustomKeywords.'com.database.DemoMySql.executeQuery'('select * from ref.AppUsers where usercode = \'Halliburton/glen.key\'')
-	String initialPW = rowsa.getString("AppPassword")
-	String initialModuser = rowsa.getString("ModUser")
-	
-	println("password: " + initialPW + " ModUser " + initialModuser)*/
-	
-	
-	WebUI.openBrowser('')
-	
-	WebUI.navigateToUrl('https://enspire-ua.gbrx.com/')
-	
-	WebUI.click(findTestObject('Login Page/Forgot Password Link'))
-	
-	WebUI.waitForPageLoad(0)
-	
-	WebUI.focus(findTestObject('Login Page/PasswordModal'), FailureHandling.STOP_ON_FAILURE)
-	
-	WebUI.setText(findTestObject('Login Page/ForgotPassword_UserName'), 'halliburton/glen.key')
-	
-	WebUI.click(findTestObject('Login Page/button_Reset Password'))
-	
-	WebUI.focus(findTestObject('Login Page/Welcome Div'), FailureHandling.STOP_ON_FAILURE)
-	
-	WebUI.focus(findTestObject('Login Page/ForgotPasswordLoginText'))
-	
-	//WebUI.verifyTextPresent('Please check your email for instructions on how to log in to your account.', false)
-/*	def rowsb = CustomKeywords.'com.database.DemoMySql.executeQuery'('select * from ref.AppUsers where usercode = \'Halliburton/glen.key\'')
-	
-	println("Rowsa " + rowsa.get(0))
-	println("Rowsb " + rowsb.get(0))*/
-} finally {
+WebUI.openBrowser('')
 
-	CustomKeywords.'com.database.DemoMySql.closeDatabaseConnection'()
-	
-	WebUI.closeBrowser()
-}
+WebUI.navigateToUrl('https://enspire-ua.gbrx.com/')
+
+WebUI.deleteAllCookies()
+
+WebUI.maximizeWindow()
+
+WebUI.click(findTestObject('Object Repository/Login Page/Forgot Password Link'))
+
+WebUI.waitForPageLoad(0)
+
+WebUI.focus(findTestObject('Object Repository/Login Page/PasswordModal'), FailureHandling.STOP_ON_FAILURE)
+
+WebUI.setText(findTestObject('Object Repository/Login Page/ForgotPassword_UserName'), 'halliburton/glen.key')
+
+WebUI.click(findTestObject('Object Repository/Login Page/button_Reset Password'))
+
+WebUI.delay(1)
+
+WebUI.verifyElementVisible(findTestObject('Object Repository/Login Page/ForgotPasswordText'), FailureHandling.CONTINUE_ON_FAILURE)
+
+WebUI.closeBrowser()
 
